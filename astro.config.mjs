625761b831bc457e1f02449e6ddf01537cbd8655
pepesrz91 <jsuarez@hey.com>
2025-11-07 14:12:52 -0600

    feat: add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: vercelServerless({}),
+});
